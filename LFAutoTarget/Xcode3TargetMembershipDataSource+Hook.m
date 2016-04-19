13330215a2fda71e9d42aecba63034b7e08a7e19
linfeng <linfeng@tuniu.com>
2016-04-19 09:57:37 +0800

    删除敏感词
    RT

--- LFAutoTarget/Xcode3TargetMembershipDataSource+Hook.m
+++ LFAutoTarget/Xcode3TargetMembershipDataSource+Hook.m
@@ -9,6 +9,11 @@
 #import "Xcode3TargetMembershipDataSource+Hook.h"
 #import "NSObject_Extension.h"
 
+#define kTarget1 @"1"
+#define kTarget2 @"2"
+#define kTarget3 @"3"
+
+
 @implementation Xcode3TargetMembershipDataSource (Hook)
 
 + (void)hook
@@ -23,7 +28,7 @@
     // 修改wrappedTarget的属性值
     NSMutableArray *wrappedTargets = [self valueForKey:@"wrappedTargets"];
     for (id wrappedTarget in wrappedTargets) {
-        if ([[wrappedTarget valueForKey:@"name"] isEqualToString:@"TuNiuApp"] || [[wrappedTarget valueForKey:@"name"] isEqualToString:@"TuNiuApp pro"] || [[wrappedTarget valueForKey:@"name"] isEqualToString:@"TuNiuApp iPad"]) {
+        if ([[wrappedTarget valueForKey:@"name"] isEqualToString:kTarget1] || [[wrappedTarget valueForKey:@"name"] isEqualToString:kTarget2] || [[wrappedTarget valueForKey:@"name"] isEqualToString:kTarget3]) {
             [wrappedTarget setValue:@YES forKey:@"selected"];
         }
     }
